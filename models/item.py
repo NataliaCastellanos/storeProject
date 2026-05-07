80b48291967530dfb522ff71c6065214952083d3
NataliaCastellanos <nmkst94@gmail.com>
2026-05-06 20:42:31 -0500

    feat: add item, product and service class structure

--- models/item.py
+++ models/item.py
@@ -1,37 +1,40 @@
 class Item:
 
-    # Constructor to initialize the item attributes
+    # Constructor
     def __init__(self, new_name, new_price, new_description):
-        self.set_name(new_name) 
+        self.set_name(new_name)
         self.set_price(new_price)
         self.description = new_description
 
-    # Getter for the item name
+    # Getter for name
     def get_name(self):
         return self._name
 
-    # Setter for the item name
+    # Setter for name
     def set_name(self, new_name):
         if(new_name != ""):
             self._name = new_name
-        else:
-            print("The item name cannot be empty")
+        else:   
+            print("The name can not be empty")
 
-    # Getter for the item price
+    # Getter for price
     def get_price(self):
         return self._price
     
-    # Setter for the item price
+    # Setter for price
+
     def set_price(self, new_price):
         if(new_price > 0):
             self._price = new_price
-        else:
+        else: 
             print("The price must be greater than 0")
 
-    # Describe method
-    def describe(self):
-        return (f"Item: {self.get_name()}, price: {self.get_price()}")
-    
-    # Calculte total
+    # Show info
+    def show_info(self):
+        return (f"Item: {self.get_name()}, price: {self.get_price()}, description: {self.description}")
+
+    # Calculate total
     def calculate_total(self):
         return self.get_price()
+    
+
--- models/service.py
+++ models/service.py
@@ -2,26 +2,26 @@ from models.item import Item
 
 class Service(Item):
 
-    #Constructor for the Service class
+    # Constructor
     def __init__(self, new_name, new_price, new_description, new_duration):
         super().__init__(new_name, new_price, new_description)
         self.set_duration(new_duration)
 
-    # Getter for the service duration    
+    # Getter for duration
     def get_duration(self):
-        return self._duration    
+        return self._duration
     
-    # Setter for the service duration
+    # Setter for duration
     def set_duration(self, new_duration):
         if(new_duration > 0):
             self._duration = new_duration
-        else:
-            print("The duration must be greater than 0")
+        else: 
+            print("The duration can not be negative")
 
-    # Describe method
-    def describe(self):
-        return (f"Service: {self.get_name()}, price: {self.get_price()}, duration: {self.get_duration()}")
+    # Show info
+    def show_info(self):
+        return (f"Service: {self.get_name()}, price: {self.get_price()}, description: {self.description}, duration: {self.get_duration()}")
     
-    # Calculte total
+     # Calculate total
     def calculate_total(self):
         return self.get_price() * self.get_duration()
