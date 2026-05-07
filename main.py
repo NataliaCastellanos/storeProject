085fcef73a6b8a41c9124fef3db64c60f781a23a
NataliaCastellanos <nmkst94@gmail.com>
2026-05-06 20:44:08 -0500

    feat: add main examples using all classes and objects

--- main.py
+++ main.py
@@ -2,104 +2,81 @@ from models.product import Product
 from models.service import Service
 from models.client import Client
 from models.employee import Employee
-from models.item import Item
+from models.cashpayment import CashPayment
+from models.cardpayment import CardPeryment
+from models.transferpayment import TransferPayment
+from models.sale import Sale
 
-# Create a product instance
-product1 = Product("Leche", 3500, "Leche entera de vaca", 100)
+productLeche = Product("Leche", 3500, "Leche entera", 10)
+productPan = Product("Pan", 1500, "", 8)
+productArroz = Product("Arroz", 7500, "Arroz blanco", 9)
 
-print("Producto 1")
-print(f"Producto: {product1.get_name()}")
-print(f"Precio: {product1.get_price()}")
-print(f"Descripción: {product1.description}")
-print(f"Stock disponible: {product1.get_stock()} \n")
+serviceLimpieza = Service("Limpieza", 50000, "Servicio de limpieza", 120)
+serviceConsultoria = Service("Consultaría", 250000, "Servicio de consultoría", 60)
 
-# Create another product instance
-product2 = Product("Pan", 1500, "Pan integral de caja", 50)
-print("Producto 2")
-print(f"Producto: {product2.get_name()}")
-print(f"Precio: {product2.get_price()}")
-print(f"Descripción: {product2.description}")
-print(f"Stock disponible: {product2.get_stock()}")
+print(productLeche.show_info())
+print(productPan.show_info())
+print(productArroz.show_info())
 
-# Create a third product instance
-product3 = Product("Huevos", 5000, "Huevos de gallina, docena", 200)
-print("\nProducto 3")
-print(f"Producto: {product3.get_name()}")
-print(f"Precio: {product3.get_price()}")
-print(f"Descripción: {product3.description}")
-print(f"Stock disponible: {product3.get_stock()}")
+print(serviceLimpieza.show_info())
+print(serviceConsultoria.show_info())
 
-# Create a fourth product instance with an empty description
-product4 = Product("Arroz", 5000, "", 80)
-print("\nProducto 4")
-print(f"Producto: {product4.get_name()}")
-print(f"Precio: {product4.get_price()}")
-print(f"Descripción: {product4.description}")
-print(f"Stock disponible: {product4.get_stock()}")
-
-# Service instances
-service1 = Service("Maintenance", 20000, "General maintenance service", 30)
-print("\nServicio 1")
-print(f"Servicio: {service1.get_name()}")
-print(f"Precio: {service1.get_price()}")
-print(f"Descripción: {service1.description}")
-print(f"Duración: {service1.get_duration()}")
-
-service2 = Service("Cleaning", 15000, "Professional cleaning service", 20)
-print("\nServicio 2")
-print(f"Servicio: {service2.get_name()}")
-print(f"Precio: {service2.get_price()}")
-print(f"Descripción: {service2.description}")
-print(f"Duración: {service2.get_duration()}")
-
-service3 = Service("Consulting", 50000, "Business consulting service", 60)
-print("\nServicio 3")
-print(f"Servicio: {service3.get_name()}")
-print(f"Precio: {service3.get_price()}")
-print(f"Descripción: {service3.description}")
-print(f"Duración: {service3.get_duration()}")
-
-#------------------------------------------------------------
-
-# Catalog
-
-catalag=[
-    Product("Leche", 3500, "Leche entera de vaca", 100), #0
-    Product("Pan", 1500, "Pan integral de caja", 50), #1
-    Service("Cleaning", 15000, "Professional cleaning service", 20)#2
+catalog = [
+    productLeche,
+    productPan, 
+    productArroz,
+    serviceConsultoria,
+    serviceLimpieza
 ]
 
-print("Catalog \n")
-for item in catalag:
-    print(f"{item.describe()}")
-    print(f"Total: {item.calculate_total()} \n")
+print("Catalog")
+for item in catalog:
+    print(item.show_info())
+    print(item.calculate_total())
 
-#Users
-users= [
-    Client("John", "Doe", "john.doe@example.com", "password123", 0),
-    Employee("Pedro", "Arias", "pedro@gmail.com", "pass123", 2000000)
+#------------------------------------------------------------------------
+clientNatalia= Client("Natalia", "Castellanos", "ncastellanos@umanizales.edu.co", "123456789", 20)
+clientMiguel = Client("Miguel", "Toro", "migueltoro@gmail.com", "987654321", 100)
+employeeSofia = Employee("Sofia", "Arias", "sofiarias@gmail.com", "741852963", 200000)
+
+users = [
+    clientNatalia,
+    clientMiguel,
+    employeeSofia
 ]
 
-print("\nUsers")
+print("\n Users")
 for user in users:
-    print(f"\n{user.show_info()}")
-    print(f"Role: {user.get_role()}")
+    print(user.show_info())
 
-# Client instances
-client1 = Client("John", "Doe", "john.doe@example.com", "password123", 0) 
+#userEmail= input("Enter your email: ")
+#userPassword = input("Enter your password: ")
 
-# employee
-employee1 = Employee("Pedro", "Arias", "pedro@gmail.com", "pass123", 2000000)
+#for user in users:
+#    print(user.login(userEmail, userPassword))
 
-print("\n LOGIN")
-# correct login
-print(client1.login("john.doe@example.com", "password123"))
+#-----------------------------------------------------------------------
 
-# incorrect login
-print(client1.login("john.doe@example.com", "password"))
-print(client1.login("john.doe@example.", "password123"))
+cashPayment1= CashPayment(14500, 20000)
+cashPayment2= CashPayment(14500, 10000)
+cardPayment1= CardPeryment(20000, "1234 5678 9101 1121")
+cardPayment2= CardPeryment(20000, "1234 5678 9101 1123")
+transferPayment1 = TransferPayment(35000, "abc123456")
+transferPayment2 = TransferPayment(35000, "abc123489")
 
-# correct login
-print(employee1.login("pedro@gmail.com", "pass123"))
-#incorrect login
-print(employee1.login("ped@gmail.com", "pass123"))
+print(cashPayment1.process_payment(True))
+print(cashPayment2.process_payment(True))
+print(cardPayment1.process_payment(True))
+print(cardPayment2.process_payment(False))
+print(transferPayment1.process_payment(True))
+print(transferPayment2.process_payment(False))
+
+print("Sale")
+sale = Sale(clientNatalia)
+
+sale.add_item(productLeche)
+sale.add_item(productPan)
+
+sale.set_payment_method(cashPayment1)
+
+sale.show_info()
